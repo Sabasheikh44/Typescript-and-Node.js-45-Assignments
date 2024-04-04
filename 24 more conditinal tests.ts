b053986688b7504c4a755a558034f7c49c39f6fa
Sabasheikh44 <162295662+Sabasheikh44@users.noreply.github.com>
2024-04-04 16:52:36 +0500

    Add files via upload

---
+++ 24 more conditinal tests.ts
@@ -0,0 +1,59 @@
+// Qyestion Number: 24. More Conditional Tests: You don’t have to limit the number of tests you create to 10. If you want to try more comparisons, write more tests. Have at least one True and one False result for each of the following:
+
+let name_1 :  string = "Areeba"
+let name_2 :  string = " Areeba Wasi"
+let name_3 : string = "Miss Areeba Wasi"
+
+// if (name_1 == name_3){
+//     console.log("names are equal")
+// } else {
+//     console.log("names are not equal")
+// }
+
+// if (name_1 != name_2) {
+//     console.log("names are equal")
+// }
+
+// if (name_1 != name_3) {
+//     console.log("names are equal")
+// }
+
+// let age_1 : number = 18
+// let age_2 : number = 20
+
+// if (age_1 == 18){
+//     console.log("eligibal for vote")
+// }
+
+// if (age_1 != 20 ){
+//     console.log("eligibal for vote in older category")
+// }
+
+// if (age_1 <= age_2){ //less
+//     console.log("younger")
+// }
+
+// if (age_2 >= age_1){ // greater
+//     console.log("older")
+// }
+
+// if (age_1 == 18 && age_2 == 20) {
+//     console.log("person is eligibal for vote")
+// }
+
+// if (age_1 = 18 || age_2 != 20) {
+//     console.log("person is eligibal not for vote")
+// }
+
+let country : string[] = ["Pakistan", "India","Japan","China"]
+// if (country.includes("Pakistan")){
+//     console.log("Pakistan is in country list")
+// }
+
+if (!country.includes("america")){
+    console.log("america is not include is an array")
+}
+
+
+
+
