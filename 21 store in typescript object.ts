d4585a99d3dbf0e03d0e5fa6e08de1e65b4bd196
Sabasheikh44 <162295662+Sabasheikh44@users.noreply.github.com>
2024-04-04 16:46:34 +0500

    Add files via upload

---
+++ 21 store in typescript object.ts
@@ -0,0 +1,4 @@
+// Question Number: 21.They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+
+let person: {name: string, fname: string, age: number,} = {name: "Areeba", fname: "Wasi",age: 17}
+console.log(person);
