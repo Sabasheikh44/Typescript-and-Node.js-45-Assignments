c2b976142b89f44ebcff6df69f85ab16eae6ec09
Sabasheikh44 <162295662+Sabasheikh44@users.noreply.github.com>
2024-04-04 16:34:01 +0500

    Add files via upload

---
+++ 13 your own arry.ts
@@ -0,0 +1,7 @@
+// / Question No: 13. Your Own Array: Thinese items, such as “I would like to own a Honda motorcycle.”
+// k of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list
+// to print a series of statements about th
+var transportation = ['Car', 'Cycle', 'Train', 'Motorcycle'];
+for (var i = 0; i < transportation.length; i++) {
+    console.log("I would like to own a " + transportation[i]);
+}
