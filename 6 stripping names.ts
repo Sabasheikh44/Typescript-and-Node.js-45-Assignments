df4ee2d149ef69ce43b684a74eeb381d76917380
Sabasheikh44 <162295662+Sabasheikh44@users.noreply.github.com>
2024-04-04 16:19:51 +0500

    Add files via upload

---
+++ 6 stripping names.ts
@@ -0,0 +1,7 @@
+// Question No : 6. Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each
+// character combination, "\t" and "\n", at least once. Print the name once, so the whitespace around the name is displayed. nt the name after striping the white 
+
+let names: string = "\t\n Saba Sheikh \t\n";
+console.log(names);
+console.log(names.trim());
+
