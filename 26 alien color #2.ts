666aef97d2ecb96813489d47fb7a1dd21028d534
Sabasheikh44 <162295662+Sabasheikh44@users.noreply.github.com>
2024-04-04 16:56:26 +0500

    Add files via upload

---
+++ 26 alien color #2.ts
@@ -0,0 +1,33 @@
+// Question Number : 25.Alien Colors #1: Imagine an alien was just shot down in a game. Create a
+// variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
+
+
+// let alien_color : string = "green"
+
+// if (alien_color == "green"){
+//     console.log("you earn 5 points")
+// }
+
+// let alien_color : string = "red"
+// if (alien_color == "green"){
+//     console.log("no output")
+// }
+
+// Question Number : 26.Alien Colors #2: Choose a color for an alien as you did in Exercise 25, and
+// write an if-else chain.
+
+// let alien_color : string = "green"
+
+//  if (alien_color == "green"){
+//     console.log("you earn 5 points for shooting the Alien")
+//  } else {
+//     console.log("player just earn 10 points")
+//  }
+
+ let alien_color : string = "red"
+
+ if (alien_color == "green"){
+    console.log("you earn 5 points for shooting the Alien")
+ } else {
+    console.log("player just earn 10 points")
+ }
