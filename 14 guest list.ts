b4ac1b77cf9c47df2a37c298b9c026233ce4a90b
Sabasheikh44 <162295662+Sabasheikh44@users.noreply.github.com>
2024-04-04 16:35:20 +0500

    Add files via upload

---
+++ 14 guest list.ts
@@ -0,0 +1,9 @@
+// Question No: 14. Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to
+// invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
+
+let Guest_List: string [] = ['Wasi khan' , 'Talha Sheikh' , 'Adil Arfi'];
+
+for(let i=0; i<Guest_List.length; i++){
+
+    console.log('Dear Mr. ' + Guest_List[i] + ',\n\nIt is our pleasure to invite you in our party.\n\nThank you!\n\n' )
+}
