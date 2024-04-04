794062fdca03316d0d3df8dd6e228ebeb7dd3109
Sabasheikh44 <162295662+Sabasheikh44@users.noreply.github.com>
2024-04-04 16:25:17 +0500

    Add files via upload

---
+++ 10 adding number.ts
@@ -0,0 +1,11 @@
+//Q uestion No: 10.Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write
+// because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence
+// describing what the program does.
+
+//Author: "Saba Sheikh";
+// Today Date: 27-3-2024;
+
+// This programme will add two digits
+
+console.log(2 + 3);
+
