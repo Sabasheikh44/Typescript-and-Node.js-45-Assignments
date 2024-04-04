b0191d9ab27164d4ebee7a08422ba8a19d1eb6a3
Sabasheikh44 <162295662+Sabasheikh44@users.noreply.github.com>
2024-04-04 16:01:29 +0500

    Add files via upload

---
+++ 2 personal message.ts
@@ -0,0 +1,5 @@
+// Question No:2 Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric,
+// //would you like to learn some typescript today?”
+
+let personName: string = "saba";
+console.log(`hello ${personName},would you like to  learn some typescript today?`);
